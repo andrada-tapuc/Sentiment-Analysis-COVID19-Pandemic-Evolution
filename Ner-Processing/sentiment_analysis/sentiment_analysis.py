4f135fcc886ab6de5f7771cad55692431e8fc0f5
andrada-tapuc <64846827+andrada-tapuc@users.noreply.github.com>
2021-06-16 00:41:56 +0300

    Update sentiment_analysis.py

--- Ner-Processing/sentiment_analysis/sentiment_analysis.py
+++ Ner-Processing/sentiment_analysis/sentiment_analysis.py
@@ -34,14 +34,8 @@ def get_sentiment_frequency(file):
     with open(file, 'r', encoding="utf-8") as f:
         reader = csv.reader(f, delimiter=',')
         lines = [line for line in reader if line != '']
-        # counts = Counter([l[1].split('-')[0] + '-' + l[1].split('-')[2] for l in lines if l !=[]])
         counts = Counter([l[3] for l in lines if l !=[]])
 
-
-    # with open(file[:-4] + "-date_frequency.csv", 'w', encoding="utf-8") as f:
-    #     writer = csv.writer(f, delimiter=',')
-    #     for i in counts:
-    #         writer.writerow([i, counts[i]])
 
     with open(file[:-4] + "-frequency.csv", 'w', encoding="utf-8") as f:
         writer = csv.writer(f, delimiter=',')
@@ -52,8 +46,6 @@ def get_sentiment_frequency(file):
 index = 0
 while index < 1:
     file = "../processing_data_code/sentiment_analysis_data/extracted_tweets" + str(index) + "_short.sentiment-analysis.csv"
-    # file = "../processing_data_code/total_hydrated/extracted_tweets" + str(index) + "_short.json"
-    # get_sentiment_frequency(file)
     get_sentiment_frequency(file)
     index += 1
     print(index)
