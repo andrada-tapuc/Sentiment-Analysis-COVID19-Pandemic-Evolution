ab314ef0f5180de7a7d6ed05323a92cc3a353fae
andrada-tapuc <64846827+andrada-tapuc@users.noreply.github.com>
2021-06-16 00:35:56 +0300

    Update app.blade.php

--- CovidAnalysis/resources/views/layouts/app.blade.php
+++ CovidAnalysis/resources/views/layouts/app.blade.php
@@ -4,15 +4,20 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Imported Styles -->
+    @section('styles')
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700|Nunito+Sans:300,300i,400,400i,600,600i,700,700i&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css" />
     <!-- Website style -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @show
+
     <title>COVID-19 Pandemic Evolution Analysis</title>
 </head>
 <body>
@@ -28,35 +33,49 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#wrap1"><span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="{{route('home')}}"><span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#wrap2">Statistics</a>
+                            <a class="nav-link" href="{{route('home')}}">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#wrap2">Contact</a>
+                            <a class="nav-link" href="{{route('statistics')}}">Statistics</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#wrap2">About</a>
+                            <a class="nav-link" href="{{route('map-page')}}">Map</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('about')}}">About</a>
                         </li>
                     </ul>
                 </div>
             </nav>
         </div>
     </header>
+    <div class="jumbotron action-wrap">
+        <video autoplay="" muted="" loop="" class="videobg">
+            <source src="{{asset ('movie/COVID19_full.mp4') }}" type="video/mp4">
+        </video>
+        <div class="video-cover"></div>
+        <div class="action-message">
+        </div>
+    </div>
     @yield('content')
-</div>
 </div>
 <footer class="container-fluid">
     <div class="row"><span class="copy">@Copyright COVID-19 Pandemic Evolution Analysis</span></div>
 </footer>
 @section('scripts')
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     <script type="application/javascript" src="{{ asset('js/script.js') }}"></script>
-{{--    <script type="application/javascript" src="{{ asset('js/main.js') }}"></script>--}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.3.2/chart.min.js"></script>
+    <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+    <script src="https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js"></script>
+    <script src="https://unpkg.com/leaflet.featuregroup.subgroup@1.0.2/dist/leaflet.featuregroup.subgroup.js"></script>
 @show
 </body>
 </html>
